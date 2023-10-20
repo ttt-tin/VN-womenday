b3c168b266fab852fe593121ebc87d13889ece2a
trongtin17 <tin.tranthanh@hcmut.edu.vn>
2023-10-20 09:15:12 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,42 @@
+
+var config = {
+    texts: [
+        "Lần đầu gặp nhau nè",      
+        "Đi chơi lần 2",  
+        "Đi ăn bò hàu quá no",
+        "Bao rạp :3",
+        "Đi coi phim nữa mà anh không nhớ ở đâu",
+        "Mua con shiba nè",
+        "Đi bảo tàng",
+        "Kỉ niệm 1 năm!!!",
+        "Đi nhạc hội",
+        "Chụp hình cho ny quá đẹp. Đố em dịp nào?",
+        "Đi thảo cầm viên nè. Xinh quá :3",
+    ],
+    imgs: {
+        "Lần đầu gặp nhau nè": "./imgs/so1.jpg",
+        "Đi chơi lần 2": "./imgs/so2.jpg",
+        "Đi ăn bò hàu quá no": "./imgs/so3.jpg",
+        "Bao rạp :3": "./imgs/so4.jpg",
+        "Đi coi phim nữa mà anh không nhớ ở đâu": "./imgs/so5.jpg",
+        "Mua con shiba nè": "./imgs/so6.jpg",
+        "Đi bảo tàng": "./imgs/so7.jpg",
+        "Kỉ niệm 1 năm!!!": "./imgs/so8.jpg",
+        "Đi nhạc hội": "./imgs/so9.jpg",
+        "Chụp hình cho ny quá đẹp. Đố em dịp nào?": "./imgs/so10.jpg",
+        "Đi thảo cầm viên nè. Xinh quá :3": "./imgs/so11.jpg",
+    },
+    
+    desc: {
+        turn_on: "Hôm nay là 20/10",
+        play: "Chúc người yêu của anh thật vui vẻ nha",
+        bannar_coming: "Cảm ơn em vì tất cả",
+        balloons_flying: "Yêu em!",
+        cake_fadein: "Tặng em nè",
+        light_candle: "Sau đây hơi cảm động đó nha",
+        wish_message: "Cùng nhìn lại xíu kỉ niệm",
+        story: "Bắt đầu nha!!!"
+    },
+
+    loveText: 'Anh yêu em'
+};
